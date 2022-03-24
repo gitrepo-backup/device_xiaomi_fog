84d75347ee0600ffe47ab1daa81eef1f01d658a6
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    fog: Inherit launch_with_vendor_ramdisk for vAB - fog has vendor_boot partition
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enable Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
