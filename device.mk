be64da95438babf3f763ac7ce3a08a944376e783
Zinadin Zidan <zidan.roking@gmail.com>
2022-04-08 21:42:47 +0700

    fog: Import bootctrl HALs and gpt-utils - Ref from https://github.com/PixelExperience-Devices/device_xiaomi_sm6150-common
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
