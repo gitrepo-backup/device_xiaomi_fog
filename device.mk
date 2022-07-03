418ba5153bf9f031a27de1a21b6d1ac8a499cb99
HeroBuxx <herobuxx@gmail.com>
2022-07-03 15:04:45 +0000

    fog: Import Keylayout config files
    From: V13.0.10.0
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -162,6 +162,12 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
